cec224ae81c1c9a3f7a1759a5e1fc50db3a04cbb
kkianamm <kiana.taktehrani@gmail.com>
2026-08-14 00:07:18 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .medtsllm import MedTsLLM
 from .medtsllm_image_fusion import MedTsLLMImageFusion
+from .medtsllm_qformer import MedTsLLMQFormer
 from .gpt4ts import GPT4TS
 
 from .dlinear import DLinear
@@ -12,6 +13,7 @@ model_lookup = {
 	"timellm": MedTsLLM,
     "medtsllm": MedTsLLM,
 	"medtsllm_image_fusion": MedTsLLMImageFusion,
+	"medtsllm_qformer": MedTsLLMQFormer,
 	"gpt4ts": GPT4TS,
     "dlinear": DLinear,
     "fedformer": FEDformer,
