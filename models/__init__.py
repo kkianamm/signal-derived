d97b657a9d3a309df655bc7c657fcc2a396b47a1
kkianamm <kiana.taktehrani@gmail.com>
2026-08-31 12:07:18 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .medtsllm import MedTsLLM
 from .medtsllm_image_fusion import MedTsLLMImageFusion
 from .medtsllm_qformer import MedTsLLMQFormer
+from .medtsllm_convnext_diagnostic_router import MedTsLLMConvNeXtDiagnosticRouter
 from .gpt4ts import GPT4TS
 
 from .dlinear import DLinear
@@ -8,13 +9,13 @@ from .FEDformer import FEDformer
 from .PatchTST import PatchTST
 from .TimesNet import TimesNet
 
-
 model_lookup = {
-	"timellm": MedTsLLM,
+    "timellm": MedTsLLM,
     "medtsllm": MedTsLLM,
-	"medtsllm_image_fusion": MedTsLLMImageFusion,
-	"medtsllm_qformer": MedTsLLMQFormer,
-	"gpt4ts": GPT4TS,
+    "medtsllm_image_fusion": MedTsLLMImageFusion,
+    "medtsllm_qformer": MedTsLLMQFormer,
+    "medtsllm_convnext_diagnostic_router": MedTsLLMConvNeXtDiagnosticRouter,
+    "gpt4ts": GPT4TS,
     "dlinear": DLinear,
     "fedformer": FEDformer,
     "patchtst": PatchTST,
